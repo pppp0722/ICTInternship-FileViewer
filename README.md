26b31506147a219f17969a3392d0e520ffcf5688
Ilhwan Lee <poj0722@naver.com>
2021-09-10 15:52:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ICTInternship-FileViewer(React + Spring + Apache)
+ICT Internship(2021) file viewer website front-end & back-end + apache server
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      hi
+    </div>
+  );
+}
+
+export default App;

b4e1bd490fc7a2fbbda24b166e3155753fb2eec1
Ilhwan Lee <poj0722@naver.com>
2021-09-13 10:43:20 +0900

    +login & session

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 
 import Login from './Login';
+import Home from './Home';
 
 const App = () => {
+  let isAuthorized = sessionStorage.getItem("isAuthorized");
+
   const Wrapper = styled.div`
-    text-align: center;
+    display: flex;
+    justify-content: center;
   `;
 
   return (
-    <Wrapper>
-      <Login/>
-    </Wrapper>
+    <BrowserRouter>
+      <Wrapper>
+        {!isAuthorized ? <Redirect to = "/"/> : <Redirect to = "/home"/>}
+        <Switch>
+          <Route exact path = "/" component = {Login}/>
+          <Route exact path = "/home" component = {Home}/>
+        </Switch>
+      </Wrapper>
+    </BrowserRouter>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,40 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import UserData from "./UserData.json";
+
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
     const Wrapper = styled.div`
-        display: inline-block;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         background-color: #BFEDCC;
         width: 1200px;
         height: 600px;
     `;
     
     const Inner = styled.div`
-        margin: 100px 0 0 0;
-        display: inline-block;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         background-color: #B6C9BB;
         width: 800px;
         height: 400px;
     `
 
+    const Context = styled.div`
+        justify-content: center;
+        align-items: center;
+    `
+
+    const Logo = styled.div`
+        padding: 15px 0 15px 0;
+        font-size: 50px;
+    `
+
     const DivInput = styled.div`
-        margin: 130px 0 0 225px;
-        width: 300px;
-        height: 130px;
+        padding: 15px 0 15px 0;
     `
 
     const Text = styled.div`
@@ -31,17 +45,20 @@ const Login = () => {
     `
 
     const Input = styled.input`
-        margin 0 0 20px 0;
-        width: 230px;
+        display: inline-block;
+        width: 240px;
         height: 30px;
         font-size: 18px;
     `;
 
     const Button = styled.button`
-        width: 90px;
-        height: 30px
+        padding: 15px 0 15px 0;
+        display: block;
+        width: 300px;
+        height: 50px;
     `;
 
+    // id, pw 두 개를 입력 받으므로 쌍으로 useState 만들어줌
     const [inputs, setInputs] = useState({
         id: "",
         pw: ""
@@ -49,6 +66,7 @@ const Login = () => {
 
     const {id, pw} = inputs;
 
+    // input form에서 문자 바뀌면 useState 바꿔줌
     const onChange = (e) =>{
         const {value, name} = e.target;
         setInputs({
@@ -57,20 +75,34 @@ const Login = () => {
         });
     };
 
-    const submit = () =>{
+    // useHistory를 사용하여 특정 라우트로 보낼 수 있음
+    const history = useHistory();
 
+    const submit = () =>{
+        if(inputs.id === UserData.id && inputs.pw === UserData.pw){
+            sessionStorage.setItem("isAuthorized","true");
+            alert("로그인 성공");
+            history.push("/home");
+        }else{
+            alert("로그인 실패");
+        }
     };
 
     return(
         <Wrapper>
             <Inner>
-                <DivInput>
-                    <Text>ID</Text>
-                    <Input name = "id" placeholder = "ID" value = {id} onChange = {onChange}/>
-                    <Text>PW</Text>
-                    <Input name = "pw" placeholder = "PW" value = {pw} onChange = {onChange}/>
-                </DivInput>
-                <Button onClick = {submit}>Login</Button>
+                <Context>
+                    <Logo>File Viewer</Logo>
+                    <DivInput>
+                        <Text>ID</Text>
+                        <Input name = "id" placeholder = "ID" value = {id} onChange = {onChange}/>
+                    </DivInput>
+                    <DivInput>
+                        <Text>PW</Text>
+                        <Input name = "pw" placeholder = "PW" value = {pw} onChange = {onChange}/>
+                    </DivInput>
+                    <Button onClick = {submit} onClick = {submit}>Login</Button>
+                </Context>
             </Inner>
         </Wrapper>
     );

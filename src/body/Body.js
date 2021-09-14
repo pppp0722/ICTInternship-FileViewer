ffaeae4aeb5bf59a576e983267f435d8ff9a6ef5
Ilhwan Lee <poj0722@naver.com>
2021-09-14 17:23:55 +0900

    +login system, server, php

--- src/body/Body.js
+++ src/body/Body.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
 import Thumbnail from './Thumbnail';
 import Detail from './Detail';
+
+import axios from 'axios';
 
 const Body = (props) => {
     // 모달창을 위해 detail 만들어 줌 true면 모달창 띄우기, false면 모달창 없애기
@@ -11,6 +13,10 @@ const Body = (props) => {
     // data를 통하여 선택한 컴포넌트의 정보를 썸네일, 모달창에 전달
     const [data, setData] = useState("");
 
+    const [text, setText] = useState("");
+
+    const [imgList, setImgList] = useState([]);
+     
     const Wrapper = styled.div`
         width: 1200px;
         padding: 20px 0 20px 0;
@@ -28,6 +34,20 @@ const Body = (props) => {
         display: inline-block;
         font-size: 50px;
     `
+    useEffect(() => {console.log(imgList)},[imgList]);
+    // 서버에 저장된 php문에서 해당 디렉토리에 있는 이미지파일의 url을 모두 전송함
+    // 이 메소드에서 모든 url을 받아와서 화면에 이미지 띄우게 할것
+    const getText = () => {
+        axios.get("http://localhost:80/image.php")
+        .then((response) => {
+            let res = response.data;
+            let strArray = res.split(',');
+
+            setImgList(strArray);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
 
     // Home 과 나머지 메뉴 분리해서
     if(props.menu === "home"){
@@ -49,6 +69,7 @@ const Body = (props) => {
                     <Thumbnail name = "5" setData = {setData} setDetail = {setDetail}/>
                     <Thumbnail name = "6" setData = {setData} setDetail = {setDetail}/>
                     <Thumbnail name = "7" setData = {setData} setDetail = {setDetail}/>
+                    <button onClick = {getText}>Get</button>
                 </Inner>
                 <Detail data = {data} detail = {detail} setDetail = {setDetail}/>
             </Wrapper>
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import UserData from "./UserData.json";
+import axios from 'axios';
 
 import { useHistory } from 'react-router-dom';
 
@@ -69,8 +70,8 @@ const Login = () => {
     `;
 
     // id, pw 두 개를 입력 받으므로 쌍으로 useState 만들어줌
-    const [id, setId] = useState("");
-    const [pw, setPw] = useState("");
+    const [id, setId] = useState();
+    const [pw, setPw] = useState();
 
     // input form에서 문자 바뀌면 useState 바꿔줌
     const handleId = (e) =>{
@@ -84,15 +85,27 @@ const Login = () => {
     // useHistory를 사용하여 특정 라우트로 보낼 수 있음
     const history = useHistory();
 
+    const success = (strArray) => {
+        if(id === strArray[0] && pw === strArray[1]){
+            sessionStorage.setItem("isAuthorized","true");
+            history.push("/");
+            alert("로그인 성공!");
+        }else{
+            alert("로그인 실패!");
+        }
+    };
+
     // id와 pw가 계정과 일치하면 권한 세션 주고 본문으로 이동 일치하지 않으면 실패
     const submit = () =>{
-        if(id === UserData.id && pw === UserData.pw){
-            sessionStorage.setItem("isAuthorized","true");
-            alert("로그인 성공");
-            history.push("/");
-        }else{
-            alert("로그인 실패");
-        }
+        axios.get("http://localhost:80/login.php")
+        .then((result) => {
+            let res = result.data
+            let strArray = res.split(',');
+
+            success(strArray);
+        }).catch((error) => {
+            console.log("can't access");
+        });
     };
 
     return(
@@ -109,7 +122,7 @@ const Login = () => {
                             <Text>PW</Text>
                             <Input type = "password" name = "pw" placeholder = "PW" value = {pw} onChange = {handlePw}/>
                         </DivInput>
-                        <Button onClick = {submit} onClick = {submit}>Login</Button>
+                        <Button onClick = {() => submit()}>Login</Button>
                     </Context>
                 </Inner>
             </Wrapper>
